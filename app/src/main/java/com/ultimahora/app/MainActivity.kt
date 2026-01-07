5b5c8b2bd53f2dbc79c82cf3761fe2fe6646d3d4
dielcampos00 <dielcampos00@gmail.com>
2026-01-07 01:10:50 -0500

    Crear app/src/main/java/com/ultimahora/app/MainActivity.kt

---
+++ app/src/main/java/com/ultimahora/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ultimahora.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Última Hora – Noticias en tiempo real")
+        }
+    }
+}
